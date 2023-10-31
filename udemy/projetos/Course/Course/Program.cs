d7dcb5769d8863d6ff335b6a33385c3b1eb91af3
Mich <michxcrz@gmail.com>
2023-10-31 15:23:22 -0300

    tipos básicos de dados em c pt 2

--- udemy/projetos/Course/Course/Program.cs
+++ udemy/projetos/Course/Course/Program.cs
@@ -4,16 +4,21 @@ namespace Course {
     class Program {
         static void Main(string[] args) {
 
-            sbyte n1 = 126;
-            int n2 = 1000;
-            int n3 = 2147483647;
-            long n4 = 214748364800L;
-
+            int n1 = int.MinValue; 
+            int n2 = int.MaxValue;
+            sbyte n3 = sbyte.MinValue;
+            sbyte n4 = sbyte.MaxValue;
+            decimal n5 = decimal.MaxValue;
+            decimal n6 = decimal.MinValue;
+            decimal n7 = 7922;
 
             Console.WriteLine(n1);
             Console.WriteLine(n2);
             Console.WriteLine(n3);
             Console.WriteLine(n4);
+            Console.WriteLine(n5);
+            Console.WriteLine(n6);
+            Console.WriteLine(n7);
         }
     }
 }
