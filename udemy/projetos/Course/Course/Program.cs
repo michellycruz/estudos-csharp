be4c73147d7cc980b4478418c3c35561d6a72b95
Mich <michxcrz@gmail.com>
2023-10-31 14:45:35 -0300

    TIPOS DE DADOS

---
+++ udemy/projetos/Course/Course/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Course {
+    class Program {
+        static void Main(string[] args) {
+
+            sbyte n1 = 126;
+            int n2 = 1000;
+            int n3 = 2147483647;
+            long n4 = 214748364800L;
+
+
+            Console.WriteLine(n1);
+            Console.WriteLine(n2);
+            Console.WriteLine(n3);
+            Console.WriteLine(n4);
+        }
+    }
+}
