6c93f7baf097ee9e1029dfd5049ce693b6447f0c
Mich <michxcrz@gmail.com>
2023-11-01 12:42:54 -0300

    SAÍDA DE DADOS

---
+++ udemy/projetos/SaidaDeDados/SaidaDeDados/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace MyApp {
+    class Program {
+        static void Main(string[] args) {
+            char genero = 'F';
+            int idade = 25;
+            double saldo = 10.26789;
+            string nome = "camila";
+
+            Console.WriteLine(nome);
+            Console.WriteLine(genero);
+            Console.WriteLine(idade);   
+            Console.WriteLine(saldo);
+            Console.WriteLine(saldo.ToString("F2"));
+            Console.WriteLine(saldo.ToString("F4"));
+        }
+    }
+}
