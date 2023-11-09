760a87ed8824588e7e07bda57b14d6e0bed88e6a
Mich <michxcrz@gmail.com>
2023-11-09 17:30:06 -0300

    estudos

--- udemy/projetos/ConversaoCasting/ConversaoCasting/Program.cs
+++ udemy/projetos/ConversaoCasting/ConversaoCasting/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program {
+        static void Main(string[] args) {
+            float x = 4.5f;
+
+            double y = x;
+            //Console.WriteLine(y);
+
+            double a;
+            float b;
+
+            // a = 5.1;
+            // b = a;  dá erro pois o double tem 8 bites e o float 4
+
+            //como definir um casting
+
+            a = 5.1;
+            b = (float)a;
+
+            Console.WriteLine(b);
+        }
+    }
+}
