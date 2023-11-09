58bbb323475d0fb7388dd9ed0887fe30ffeb0101
Mich <michxcrz@gmail.com>
2023-11-09 17:40:07 -0300

    como fazer um casting

--- udemy/projetos/ConversaoCasting/ConversaoCasting/Program.cs
+++ udemy/projetos/ConversaoCasting/ConversaoCasting/Program.cs
@@ -9,18 +9,34 @@ namespace MyApp // Note: actual namespace depends on the project name.
             double y = x;
             //Console.WriteLine(y);
 
-            double a;
-            float b;
+            //double a;
+            //float b;
 
             // a = 5.1;
             // b = a;  dá erro pois o double tem 8 bites e o float 4
 
             //como definir um casting
 
-            a = 5.1;
-            b = (float)a;
+            //a = 5.1;
+            //b = (float)a;
 
-            Console.WriteLine(b);
+            //Console.WriteLine(b);
+
+            //outro exemplo 
+
+            //double a;
+            //int b;
+
+            //a = 5.1;
+            //b = (int)a; //é arredondado pra 5
+
+            //Console.WriteLine(b);
+
+            int a = 5;
+            int b = 2;
+
+            double resultado = (double)a / b;
+            Console.WriteLine(resultado);
         }
     }
 }
