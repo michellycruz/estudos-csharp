4832ed57aa5d5491b09e494404772e912bf6350c
Mich <michxcrz@gmail.com>
2023-11-13 10:46:34 -0300

    curso de cu

--- udemy/projetos/Course/Course/Program.cs
+++ udemy/projetos/Course/Course/Program.cs
@@ -4,21 +4,8 @@ namespace Course {
     class Program {
         static void Main(string[] args) {
 
-            int n1 = int.MinValue;
-            int n2 = int.MaxValue;
-            sbyte n3 = sbyte.MinValue;
-            sbyte n4 = sbyte.MaxValue;
-            decimal n5 = decimal.MaxValue;
-            decimal n6 = decimal.MinValue;
-            decimal n7 = 7922;
-
+            double n1 = (double)10 / 8;
             Console.WriteLine(n1);
-            Console.WriteLine(n2);
-            Console.WriteLine(n3);
-            Console.WriteLine(n4);
-            Console.WriteLine(n5);
-            Console.WriteLine(n6);
-            Console.WriteLine(n7);
         }
     }
 }
