5c13e578d249bbf76fa8a69c3fc5c333dd7639f6
Mich <michxcrz@gmail.com>
2023-11-13 13:49:05 -0300

    curso de cu

--- udemy/projetos/Course/Course/Program.cs
+++ udemy/projetos/Course/Course/Program.cs
@@ -4,16 +4,29 @@ namespace Course {
     class Program {
         static void Main(string[] args) {
 
-            double a = 1.0, b = -3.0, c = -4.0;
+            //double a = 1.0, b = -3.0, c = -4.0;
 
-            double delta = Math.Pow(b, 2.0) - 4.0 * a * c;
+            //double delta = Math.Pow(b, 2.0) - 4.0 * a * c;
 
-            double x1 = (-b + Math.Sqrt(delta)) / (2.0 * a);
-            double x2 = (-b - Math.Sqrt(delta)) / (2.0 * a);
+            //double x1 = (-b + Math.Sqrt(delta)) / (2.0 * a);
+            //double x2 = (-b - Math.Sqrt(delta)) / (2.0 * a);
 
-            Console.WriteLine(delta);
-            Console.WriteLine(x1);
-            Console.WriteLine(x2);
+            //Console.WriteLine(delta);
+            //Console.WriteLine(x1);
+            //Console.WriteLine(x2);
+
+
+            string frase = Console.ReadLine();
+            string x = Console.ReadLine();
+            string y = Console.ReadLine();
+            string z = Console.ReadLine();
+
+            Console.WriteLine("Você digitou: ");
+            Console.WriteLine(frase);
+            Console.WriteLine(x);
+            Console.WriteLine(y);
+            Console.WriteLine(z);
+
         }
     }
 }
