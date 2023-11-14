7775580fe5e4500dce709a3aa0f2d37fa1a762cc
Mich <michxcrz@gmail.com>
2023-11-14 15:50:25 -0300

    EXERCICIO 2

--- udemy/projetos/Course/Course/Program.cs
+++ udemy/projetos/Course/Course/Program.cs
@@ -1,15 +1,24 @@
 ﻿using System;
+using System.Globalization;
 
 namespace Course {
     class Program {
         static void Main(string[] args) {
 
-            Console.WriteLine("Informe o primeiro número");
-            int n1 = int.Parse(Console.ReadLine());
-            Console.WriteLine("Informe o segundo número");
-            int n2 = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Informe o primeiro número");
+            //int n1 = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Informe o segundo número");
+            //int n2 = int.Parse(Console.ReadLine());
 
-            Console.WriteLine("SOMA = " + (n1 + n2));
+            //Console.WriteLine("SOMA = " + (n1 + n2));
+
+            double raio, area, pi = 3.14159;
+            Console.WriteLine("Informe o valor do raio: ");
+            raio = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            area = pi * raio * raio;
+
+            Console.WriteLine("Área igual a " +  area.ToString("F4", CultureInfo.InvariantCulture));
         }
     }
 }
