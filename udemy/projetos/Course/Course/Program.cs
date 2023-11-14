1d993b5f873e7fa532e950bad4e5f86c444cdf4a
Mich <michxcrz@gmail.com>
2023-11-14 14:46:54 -0300

    atividades c#

--- udemy/projetos/Course/Course/Program.cs
+++ udemy/projetos/Course/Course/Program.cs
@@ -4,29 +4,12 @@ namespace Course {
     class Program {
         static void Main(string[] args) {
 
-            //double a = 1.0, b = -3.0, c = -4.0;
+            Console.WriteLine("Informe o primeiro número");
+            int n1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Informe o segundo número");
+            int n2 = int.Parse(Console.ReadLine());
 
-            //double delta = Math.Pow(b, 2.0) - 4.0 * a * c;
-
-            //double x1 = (-b + Math.Sqrt(delta)) / (2.0 * a);
-            //double x2 = (-b - Math.Sqrt(delta)) / (2.0 * a);
-
-            //Console.WriteLine(delta);
-            //Console.WriteLine(x1);
-            //Console.WriteLine(x2);
-
-
-            string frase = Console.ReadLine();
-            string x = Console.ReadLine();
-            string y = Console.ReadLine();
-            string z = Console.ReadLine();
-
-            Console.WriteLine("Você digitou: ");
-            Console.WriteLine(frase);
-            Console.WriteLine(x);
-            Console.WriteLine(y);
-            Console.WriteLine(z);
-
+            Console.WriteLine("SOMA = " + (n1 + n2));
         }
     }
 }
