ee4d59e3a41d480ae225a94b0fdc36aee7a4f9de
Mich <michxcrz@gmail.com>
2023-11-14 16:35:00 -0300

    exercicio 5

--- udemy/projetos/Course/Course/Program.cs
+++ udemy/projetos/Course/Course/Program.cs
@@ -5,6 +5,8 @@ namespace Course {
     class Program {
         static void Main(string[] args) {
 
+            // ------------------------------------- EXERCICIO 1
+
             //Console.WriteLine("Informe o primeiro número");
             //int n1 = int.Parse(Console.ReadLine());
             //Console.WriteLine("Informe o segundo número");
@@ -12,13 +14,49 @@ namespace Course {
 
             //Console.WriteLine("SOMA = " + (n1 + n2));
 
-            double raio, area, pi = 3.14159;
-            Console.WriteLine("Informe o valor do raio: ");
-            raio = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            // ------------------------------------- EXERCICIO 2
 
-            area = pi * raio * raio;
+            //double raio, area, pi = 3.14159;
+            //Console.WriteLine("Informe o valor do raio: ");
+            //raio = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
 
-            Console.WriteLine("Área igual a " +  area.ToString("F4", CultureInfo.InvariantCulture));
+            //area = pi * raio * raio;
+
+            //Console.WriteLine("Área igual a " +  area.ToString("F4", CultureInfo.InvariantCulture));
+
+            // ------------------------------------- EXERCICIO 3
+
+            //Console.WriteLine("Valor de A: ");
+            //int a = int.Parse(Console.ReadLine());
+
+            //Console.WriteLine("Valor de B: ");
+            //int b = int.Parse(Console.ReadLine());
+
+            //Console.WriteLine("Valor de C: ");
+            //int c = int.Parse(Console.ReadLine());
+
+            //Console.WriteLine("Valor de D: ");
+            //int d = int.Parse(Console.ReadLine());
+
+            //int diferenca = (a * b - c * d);
+
+            //Console.WriteLine("DIFERENÇA = " + diferenca);
+
+            // ------------------------------------- EXERCICIO 4
+
+            Console.WriteLine("ID do funcionário: ");
+            int idFuncionario = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Número de horas trabalhadas");
+            double horasTrabalhadas = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Valor que recebe por hora: ");
+            double valorPorHora = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double salario = horasTrabalhadas * valorPorHora;
+
+            Console.WriteLine("NUMBER: " + idFuncionario);
+            Console.WriteLine("SALARY: U$ " + salario.ToString("F2", CultureInfo.InvariantCulture));
         }
     }
 }
