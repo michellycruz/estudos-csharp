5bb3d841eda2e611d58c0de05a613d7718efc52d
Mich <michxcrz@gmail.com>
2023-11-16 13:15:10 -0300

    exercicio 6 completo

--- udemy/projetos/Course/Course/Program.cs
+++ udemy/projetos/Course/Course/Program.cs
@@ -60,30 +60,52 @@ namespace Course {
 
             // ------------------------------------- EXERCICIO 5
 
-            int codigoPeca1, codigoPeca2, quantidadePeca1, quantidadePeca2;
-            double preco1, preco2, total;
+            //int codigoPeca1, codigoPeca2, quantidadePeca1, quantidadePeca2;
+            //double preco1, preco2, total;
 
-            Console.WriteLine("------------------ PRODUTO 1 -------------------");
-            Console.WriteLine("INFORME O CÓDIGO DO PRODUTO, QUANTIDADE E PREÇO POR UNIDADE");
+            //Console.WriteLine("--- PRODUTO 1 ---");
+            //Console.WriteLine("INFORME O CÓDIGO DO PRODUTO, QUANTIDADE E PREÇO POR UNIDADE");
 
-            string[] valores = Console.ReadLine().Split(' ');
-            codigoPeca1 = int.Parse(valores[0]);
-            quantidadePeca1 = int.Parse(valores[1]);
-            preco1 = double.Parse(valores[2], CultureInfo.InvariantCulture);
+            //string[] valores = Console.ReadLine().Split(' ');
+            //codigoPeca1 = int.Parse(valores[0]);
+            //quantidadePeca1 = int.Parse(valores[1]);
+            //preco1 = double.Parse(valores[2], CultureInfo.InvariantCulture);
 
 
-            Console.WriteLine("------------------ PRODUTO 2 -------------------");
-            Console.WriteLine("INFORME O CÓDIGO DO PRODUTO, QUANTIDADE E PREÇO POR UNIDADE");
+            //Console.WriteLine("--- PRODUTO 2 ---");
+            //Console.WriteLine("INFORME O CÓDIGO DO PRODUTO, QUANTIDADE E PREÇO POR UNIDADE");
 
-            valores = Console.ReadLine().Split(' ');
-            codigoPeca2 = int.Parse(valores[0]);
-            quantidadePeca2 = int.Parse(valores[1]);
-            preco2 = double.Parse(valores[2], CultureInfo.InvariantCulture);
+            //valores = Console.ReadLine().Split(' ');
+            //codigoPeca2 = int.Parse(valores[0]);
+            //quantidadePeca2 = int.Parse(valores[1]);
+            //preco2 = double.Parse(valores[2], CultureInfo.InvariantCulture);
 
-            total = preco1 * quantidadePeca1 + preco2 * quantidadePeca2;
+            //total = preco1 * quantidadePeca1 + preco2 * quantidadePeca2;
 
-            Console.WriteLine("------------------ VALOR TOTAL -------------------");
-            Console.WriteLine("VALOR A PAGAR: R$" + total.ToString("F2", CultureInfo.InvariantCulture));
+            //Console.WriteLine("--- VALOR TOTAL ---");
+            //Console.WriteLine("VALOR A PAGAR: R$" + total.ToString("F2", CultureInfo.InvariantCulture));
+
+            // ------------------------------------- EXERCICIO 6
+
+            double a, b, c, triangulo, circulo, trapezio, quadrado, retangulo;
+
+            Console.WriteLine("Informe o valor de A, B e C:");
+            String[] valores = Console.ReadLine().Split(' ');
+            a = double.Parse(valores[0], CultureInfo.InvariantCulture);
+            b = double.Parse(valores[1], CultureInfo.InvariantCulture);
+            c = double.Parse(valores[2], CultureInfo.InvariantCulture);
+
+            triangulo = a * c / 2;
+            circulo = 3.14159 * c * c;
+            trapezio = (a + b) / 2.0 * c;
+            quadrado = b * b;
+            retangulo = a * b;
+
+            Console.WriteLine("TRIÂNGULO: " + triangulo.ToString("F3", CultureInfo.InvariantCulture) +
+                "\nCIRCULO: " + circulo.ToString("F3", CultureInfo.InvariantCulture) +
+                "\nTRAPÉZIO: " + trapezio.ToString("F3", CultureInfo.InvariantCulture) +
+                "\nQUADRADO: " + quadrado.ToString("F3", CultureInfo.InvariantCulture) +
+                "\nRETÂNGULO: " + retangulo.ToString("F3", CultureInfo.InvariantCulture));
         }
     }
 }
