421346ce81015ff87cba9e2985c0baf4e800b565
Mich <michxcrz@gmail.com>
2023-11-17 17:17:15 -0300

    ?

--- udemy/projetos/Course/Course/Program.cs
+++ udemy/projetos/Course/Course/Program.cs
@@ -220,25 +220,27 @@ namespace Course {
 
             // ------------------------------------- PARTE 2 - EXERCICIO 6
 
-            Console.WriteLine("Informe um valor");
+            //Console.WriteLine("Informe um valor");
 
-            double numero = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            //double numero = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
 
-            if (numero < 0.0 || numero > 100.0) {
-                Console.WriteLine("Fora de intervalo");
-            }
-            else if (numero <= 25.0) {
-                Console.WriteLine("Intervalo [0, 25]");
-            }
-            else if (numero <= 50.0) {
-                Console.WriteLine("Intervalo [25, 50]");
-            }
-            else if (numero <= 75.0) {
-                Console.WriteLine("Intervalo [50, 75]");
-            }
-            else {
-                Console.WriteLine("Intervalo [75, 100]");
-            }
+            //if (numero < 0.0 || numero > 100.0) {
+            //    Console.WriteLine("Fora de intervalo");
+            //}
+            //else if (numero <= 25.0) {
+            //    Console.WriteLine("Intervalo [0, 25]");
+            //}
+            //else if (numero <= 50.0) {
+            //    Console.WriteLine("Intervalo [25, 50]");
+            //}
+            //else if (numero <= 75.0) {
+            //    Console.WriteLine("Intervalo [50, 75]");
+            //}
+            //else {
+            //    Console.WriteLine("Intervalo [75, 100]");
+            //}
+
+            // ------------------------------------- PARTE 2 - EXERCICIO 7
         }
     }
 }
