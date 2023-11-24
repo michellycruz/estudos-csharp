42d728aa662d523b19fd8a95badaf3156859bd9b
Mich <michxcrz@gmail.com>
2023-11-24 12:57:57 -0300

    ?

--- udemy/projetos/Course/Course/Program.cs
+++ udemy/projetos/Course/Course/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Globalization;
 using System.Runtime.Serialization;
+using System.Xml.Linq;
 
 namespace Course {
     class Program {
@@ -542,6 +543,37 @@ namespace Course {
             //    Console.WriteLine($"{valor1} {valor2} {valor3}");
             //}
 
+            // ------------------------------------- AULA 38 - RESOLVENDO O PROBLEMA SEM ORIENTAÇÃO A OBJETOS
+
+            //double xA, xB, xC, yA, yB, yC;
+
+            //Console.WriteLine("Entre com as medidas do triângulo X: ");
+            //xA = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            //xB = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            //xC = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            //Console.WriteLine("Entre com as medidas do triângulo Y: ");
+            //yA = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            //yB = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            //yC = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            //double p = (xA + xB + xC) / 2.0;
+            //double areaX = Math.Sqrt(p * (p - xA) * (p - xB) * (p - xC));
+
+            //p = (yA + yB + yC) / 2.0;
+            //double areaY = Math.Sqrt(p * (p - yA) * (p - yB) * (p - yC));
+
+            //Console.WriteLine("Área de x = " + areaX.ToString("F4", CultureInfo.InvariantCulture));
+            //Console.WriteLine("Área de y = " + areaY.ToString("F4", CultureInfo.InvariantCulture));
+
+            //if (areaX > areaY) {
+            //    Console.WriteLine("Maior área: X");
+            //}
+            //else {
+            //    Console.WriteLine("Maior área: Y");
+            //}
+
+            // ------------------------------------- AULA 39  RESOLVENDO O PROBLEMA SEM ORIENTAÇÃO A OBJETOS
         }
     }
 }
