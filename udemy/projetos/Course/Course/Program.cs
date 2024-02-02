7f1b58ee28b188366e5a4c5112433c3cb75c10f3
Mich <michxcrz@gmail.com>
2024-02-02 14:26:39 -0300

    ?

--- udemy/projetos/Course/Course/Program.cs
+++ udemy/projetos/Course/Course/Program.cs
@@ -575,36 +575,46 @@ namespace Course {
 
             // ------------------------------------- AULA 39 - CRIANDO UMA CLASSE COM TRÊS ATRIBUTOS
 
-            Triangulo x, y;
+            //Triangulo x, y;
 
-            x = new Triangulo();
-            y = new Triangulo();
+            //x = new Triangulo();
+            //y = new Triangulo();
 
-            Console.WriteLine("Entre com as medidas do triângulo X: ");
-            x.A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-            x.B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-            x.C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            //Console.WriteLine("Entre com as medidas do triângulo X: ");
+            //x.A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            //x.B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            //x.C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
 
-            Console.WriteLine("Entre com as medidas do triângulo Y: ");
-            y.A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-            y.B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-            y.C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            //Console.WriteLine("Entre com as medidas do triângulo Y: ");
+            //y.A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            //y.B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            //y.C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
 
-            double p = (x.A + x.B + x.C) / 2.0;
-            double areaX = Math.Sqrt(p * (p - x.A) * (p - x.B) * (p - x.C));
+            //double p = (x.A + x.B + x.C) / 2.0;
+            //double areaX = Math.Sqrt(p * (p - x.A) * (p - x.B) * (p - x.C));
 
-            p = (y.A + y.B + y.C) / 2.0;
-            double areaY = Math.Sqrt(p * (p - y.A) * (p - y.B) * (p - y.C));
+            //p = (y.A + y.B + y.C) / 2.0;
+            //double areaY = Math.Sqrt(p * (p - y.A) * (p - y.B) * (p - y.C));
 
-            Console.WriteLine("Área de x = " + areaX.ToString("F4", CultureInfo.InvariantCulture));
-            Console.WriteLine("Área de y = " + areaY.ToString("F4", CultureInfo.InvariantCulture));
+            //Console.WriteLine("Área de x = " + areaX.ToString("F4", CultureInfo.InvariantCulture));
+            //Console.WriteLine("Área de y = " + areaY.ToString("F4", CultureInfo.InvariantCulture));
 
-            if (areaX > areaY) {
-                Console.WriteLine("Maior área: X");
-            }
-            else {
-                Console.WriteLine("Maior área: Y");
-            }
+            //if (areaX > areaY) {
+            //    Console.WriteLine("Maior área: X");
+            //}
+            //else {
+            //    Console.WriteLine("Maior área: Y");
+            //}
+
+            // ------------------------------------- PRIMEIROS EXERCÍCIOS - EXERCÍCIO 01
+            //Fazer um programa para ler os dados de duas pessoas, depois mostrar o nome da pessoa mais velha.
+
+            Pessoas Nome, Idade;
+
+            Nome = new Pessoas();
+            Idade = new Pessoas();
+
+            Console.WriteLine(Console.ReadLine());
         }
     }
 }
