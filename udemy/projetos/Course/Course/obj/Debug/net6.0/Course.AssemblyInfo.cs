ea007228c036b67ee4c345af18120abecda3ceb3
Mich <125368412+michellycruz@users.noreply.github.com>
2024-02-04 20:39:12 -0300

    criando metodos separados

--- udemy/projetos/Course/Course/obj/Debug/net6.0/Course.AssemblyInfo.cs
+++ udemy/projetos/Course/Course/obj/Debug/net6.0/Course.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Course")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7a8be846f81db495f1fff4becd6dddb77761f1cc")]
 [assembly: System.Reflection.AssemblyProductAttribute("Course")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Course")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
