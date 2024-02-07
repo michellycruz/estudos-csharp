5e20ca8132cd6abafd0b9417fb366e76640a7088
Mich <125368412+michellycruz@users.noreply.github.com>
2024-02-06 23:11:30 -0300

    ?

--- udemy/projetos/Course/Course/Program.cs
+++ udemy/projetos/Course/Course/Program.cs
@@ -660,34 +660,34 @@ namespace Course {
 
             // ------------------------------------- AULA 41 - CRIANDO UM MÉTODO PARA OBTERMOS OS BENEFÍCIOS DE REAPROVEITAMENTO E DELEGAÇÃO
 
-            Triangulo x, y;
+            //Triangulo x, y;
 
-            x = new Triangulo();
-            y = new Triangulo();
+            //x = new Triangulo();
+            //y = new Triangulo();
 
-            Console.WriteLine("Entre com as medidas do triângulo X: ");
-            x.A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-            x.B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-            x.C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            //Console.WriteLine("Entre com as medidas do triângulo X: ");
+            //x.A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            //x.B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            //x.C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
 
-            Console.WriteLine("Entre com as medidas do triângulo Y: ");
-            y.A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-            y.B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-            y.C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            //Console.WriteLine("Entre com as medidas do triângulo Y: ");
+            //y.A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            //y.B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            //y.C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
 
-            double areaX = x.Area();
+            //double areaX = x.Area();
 
-            double areaY = y.Area();
+            //double areaY = y.Area();
 
-            Console.WriteLine("Área de x = " + areaX.ToString("F4", CultureInfo.InvariantCulture));
-            Console.WriteLine("Área de y = " + areaY.ToString("F4", CultureInfo.InvariantCulture));
+            //Console.WriteLine("Área de x = " + areaX.ToString("F4", CultureInfo.InvariantCulture));
+            //Console.WriteLine("Área de y = " + areaY.ToString("F4", CultureInfo.InvariantCulture));
 
-            if (areaX > areaY) {
-                Console.WriteLine("Maior área: X");
-            }
-            else {
-                Console.WriteLine("Maior área: Y");
-            }
+            //if (areaX > areaY) {
+            //    Console.WriteLine("Maior área: X");
+            //}
+            //else {
+            //    Console.WriteLine("Maior área: Y");
+            //}
         }
     }
 }
